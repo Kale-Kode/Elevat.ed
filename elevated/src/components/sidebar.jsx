527e1f6b9256de19ba8cf323a1f0b7b11cfa6cef
Kale-kode <kaileliu2017@gmail.com>
2025-03-23 11:05:34 +0000

    sidebar navigation setup

---
+++ elevated/src/components/sidebar.jsx
@@ -0,0 +1,55 @@
+import { Home, BookOpen, Briefcase, Activity, Bookmark, Users, Settings, LogOut } from "lucide-react";
+
+const Sidebar = ({ selected, setSelected }) => {
+
+    const menuItems = [
+        { id: "home", label: "Home", icon: <Home size={20} /> },
+        { id: "learn", label: "Learn", icon: <BookOpen size={20} /> },
+        { id: "project", label: "Project", icon: <Briefcase size={20} /> },
+        { id: "cas", label: "CAS", icon: <Activity size={20} /> },
+        { id: "trackr", label: "Trackr", icon: <Bookmark size={20} /> },
+        { id: "mentors", label: "Mentors", icon: <Users size={20} /> },
+        { id: "settings", label: "Settings", icon: <Settings size={20} /> },
+    ];
+
+    async function signOut() {
+        const { error } = await supabase.auth.signOut()
+        if (error){
+        console.log('error signing out: ', error)
+        }
+    }
+
+    return (
+        <div className="w-64 bg-white border-r min-h-screen flex flex-col items-spread gap-4 py-8 rounded-r-lg">
+            <div className="flex gap-2 items-center justify-center">
+                <img className="w-10" src='/elevated_logo.png' alt='logo'></img>
+                <p className="text-3xl font-bold text-green-full">Elevat.ed</p>
+            </div>
+
+            <img src='profile.png' className="w-16 mx-auto"></img>
+
+            <nav className="flex flex-col">
+                {menuItems.map((item) => (
+                <div
+                    key={item.id}
+                    className={`flex items-center gap-3 w-full pl-12 p-3 cursor-pointer hover:bg-blue-light transition-all ${
+                    selected === item.id ? "bg-blue-light text-blue-full border-l-4 border-blue-full" : "text-gray-600"
+                    }`}
+                    onClick={() => setSelected(item.id)}>
+                    {item.icon}
+                    <p>{item.label}</p>
+                </div>
+                ))}
+            </nav>
+
+            <div className="flex items-center gap-3 p-3 pl-12 rounded-lg cursor-pointer text-red-500 hover:bg-red-100 mt-auto"
+                onClick={signOut}>
+                <LogOut size={20} />
+                <span>Sign Out</span>
+            </div>
+
+        </div>
+    );
+}
+
+export default Sidebar
--- elevated/src/pages/Dashboard.jsx
+++ elevated/src/pages/Dashboard.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { supabase } from "../integrations/supabase/client";
+import  Sidebar from '../components/sidebar';
+import { useState } from 'react';
 
 const Dashboard = () => {
-  async function signOut() {
-    const { error } = await supabase.auth.signOut()
-    if (error){
-      console.log('error signing out: ', error)
+  const [selected, setSelected] = useState('home');
+
+  const renderView = () => {
+    switch (selected) {
+      case 'home':
+        return <div>Home View</div>;
+      case 'learn':
+        return <div>Profile View</div>;
+      case 'project':
+        return <div>Settings View</div>;
+      case 'cas':
+        return <div>About View</div>;
+      case 'trackr':
+        return <div>About View</div>;
+      case 'mentors':
+        return <div>About View</div>;
+      case 'settings':
+        return <div>About View</div>;
     }
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Dashboard Page</h1>
-      <button onClick={signOut}>Sign Out</button>
+    <div className="flex w-screen h-screen overflow-hidden">
+
+      <Sidebar selected={selected} setSelected={setSelected}/>
+      
+      <div className='w-full h-full px-12 py-6'>
+        {renderView()}
+      </div>
+        
     </div>
   ) 
+
 }
 
 export default Dashboard
